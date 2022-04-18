7b4c3d1280583b7ad748da50a7b4c97337730f0e
tung.pham <tung.pham@mission.plus>
2022-04-18 16:30:03 +0700

    Create Delete Post

---
+++ app/routes/posts/admin/$slug.tsx
@@ -0,0 +1,47 @@
+import { marked } from "marked";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import { deletePost, Post } from "~/models/post.server";
+import { getPost } from "~/models/post.server";
+
+type LoaderData = { post: Post; html: string };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  const html = marked(post.markdown);
+  return json<LoaderData>({ post, html });
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  invariant(params.slug, "slug not found");
+
+  await deletePost({ slug: params.slug });
+
+  return redirect("/posts/admin");
+};
+
+export default function PostSlug() {
+  const { post, html } = useLoaderData() as LoaderData;
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <hr className="my-4" />
+      <Form method="post">
+        <button
+          type="submit"
+          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+        >
+          Delete
+        </button>
+      </Form>
+    </main>
+  );
+}
